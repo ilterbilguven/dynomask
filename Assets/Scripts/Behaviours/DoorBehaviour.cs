7a11a44815570fca1173595ad61681d02c13950f
Filippo Maria Perlini <filippomariaperlini@gmail.com>
2026-01-31 19:27:41 +0100

    Door behaviour

---
+++ Assets/Scripts/Behaviours/DoorBehaviour.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace Game
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class DoorBehaviour : MonoBehaviour
+    {
+        // -1 will automatically go to next level..
+        [SerializeField] private int m_NextRoomIndex = -1;
+        private static readonly string PlayerTag = "Player";
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag(PlayerTag))
+            {
+                LevelManager.Instance.ChangeLevel(m_NextRoomIndex + 1);
+            }
+        }
+    }
+}
