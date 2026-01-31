13c8a6538b61951644705dc070971299f9f30b34
Filippo Maria Perlini <filippomariaperlini@gmail.com>
2026-01-31 14:45:46 +0100

    Dimension fixes

--- Assets/Scripts/DimensionObject.cs
+++ Assets/Scripts/DimensionObject.cs
@@ -1,3 +1,4 @@
+using System;
 using Game.Managers;
 using UnityEngine;
 
@@ -5,11 +6,16 @@ namespace Game
 {
     public class DimensionObject : MonoBehaviour
     {
-        [SerializeField] private int m_DimensionIndex = 0;
+        [SerializeField] private Timeline m_TimelineExistance = Timeline.Past | Timeline.Present | Timeline.Future;
+        [SerializeField] private bool m_ValidateMovements = true;
+        [SerializeField] private bool m_DisableOnWrongTimeline = false;
+
+        private Vector2 m_LastValidPosition;
         
         void Start()
         {
             DimensionManager.Instance.OnDimensionChange?.AddListener(OnDimensionChanged);
+            m_LastValidPosition = transform.position;
         }
 
         private void OnDestroy()
@@ -17,9 +23,21 @@ namespace Game
             DimensionManager.Instance.OnDimensionChange?.RemoveListener(OnDimensionChanged);
         }
 
-        private void OnDimensionChanged(int index)
+        private void OnDimensionChanged(Timeline from, Timeline to)
         {
-            gameObject.SetActive(m_DimensionIndex == index);
+            if(m_ValidateMovements)
+                ValidatePosition(from, to);
+            if(m_DisableOnWrongTimeline)
+                gameObject.SetActive(m_TimelineExistance.HasFlag(to));
+        }
+
+        private void ValidatePosition(Timeline from, Timeline to)
+        {
+            bool valid = (int)from <= (int)to;
+            if (valid)
+                m_LastValidPosition = transform.position;
+            else
+                transform.position = m_LastValidPosition;
         }
     }
 }
--- Assets/Scripts/Managers/DimensionManager.cs
+++ Assets/Scripts/Managers/DimensionManager.cs
@@ -4,22 +4,37 @@ using UnityEngine.Events;
 
 namespace Game.Managers
 {
+    [Flags]
+    public enum Timeline { Past = 2, Present = 4, Future = 8 }
+    
     public class DimensionManager : SingletonBehaviour<DimensionManager>
     {
-        public UnityEvent<int> OnDimensionChange { get; private set; }
-        private int m_CurrentDimension = 0;
-        public int CurrentDimension => m_CurrentDimension;
+        public UnityEvent<Timeline, Timeline> OnDimensionChange { get; private set; }
+        private Timeline m_CurrentDimension = Timeline.Present;
+        public Timeline CurrentDimension => m_CurrentDimension;
         
         protected override void Awake()
         {
             base.Awake();
-            OnDimensionChange = new UnityEvent<int>();
+            OnDimensionChange = new UnityEvent<Timeline, Timeline>();
         }
 
-        public void SetDimension(int index)
+        // public Timeline test;
+        // public bool change;
+        // private void Update()
+        // {
+        //     if (change)
+        //     {
+        //         change = false;
+        //         SetDimension(test);
+        //     }
+        // }
+
+        public void SetDimension(Timeline to)
         {
-            m_CurrentDimension = index;
-            OnDimensionChange?.Invoke(index);
+            Timeline fromTimeline = m_CurrentDimension;
+            m_CurrentDimension = to;
+            OnDimensionChange?.Invoke(fromTimeline, to);
         }
     }
 }
