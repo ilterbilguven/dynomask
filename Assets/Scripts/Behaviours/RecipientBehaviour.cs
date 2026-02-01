23443cb86d73c3f36b7601544e29c686c1bdd220
İlter Bilgüven <ilter@live.com>
2026-02-01 15:20:25 +0100

    feat: recipient door can take multiple activations

--- Assets/Scripts/Behaviours/RecipientBehaviour.cs
+++ Assets/Scripts/Behaviours/RecipientBehaviour.cs
@@ -6,17 +6,23 @@ namespace Game.Behaviours
     public class RecipientBehaviour : MonoBehaviour
     {
         [ReadOnly, SerializeField] private bool _activated;
+        [ReadOnly, SerializeField] private int _currentActivationCount = 0;
+
+        [SerializeField] private int _requiredActivations = 1;
+        
         
         public void Activate()
         {
-            _activated = true;
-            gameObject.SetActive(false);
+            _currentActivationCount++;
+            _activated = _currentActivationCount == _requiredActivations;
+            gameObject.SetActive(!_activated);
         }
 
         public void Deactivate()
         {
-            _activated = false;
-            gameObject.SetActive(true);
+            _currentActivationCount--;
+            _activated = _currentActivationCount == _requiredActivations;
+            gameObject.SetActive(!_activated);
         }
     }
 }
