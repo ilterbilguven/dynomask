8923e27f4b80625012b9bfbdeca4a650d0cbc357
İlter Bilgüven <ilter@live.com>
2026-02-01 16:16:41 +0100

    feat: autostart

--- Assets/Scripts/UI/MainMenuPanelBehaviour.cs
+++ Assets/Scripts/UI/MainMenuPanelBehaviour.cs
@@ -1,9 +1,14 @@
+using System;
+using Game.Managers;
 using UnityEngine;
 
 namespace Game.UI
 {
     public class MainMenuPanelBehaviour : UIPanelBehaviour
     {
-        
+        private void Start()
+        {
+            GameManager.Instance.StartGame();
+        }
     }
 }
